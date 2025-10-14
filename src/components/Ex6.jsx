993c22df2538d5f9e5a95dba0fe2bbd6051784b1
Leonardo Lopes Pereira <leonardolopespereira7@gmail.com>
2025-10-14 20:59:05 -0300

    ex6 realizado

--- src/components/Ex6.jsx
+++ src/components/Ex6.jsx
@@ -12,7 +12,11 @@ export default function Ex6() {
   return (
     <div className="my-8">
         <h1 className="text-2xl font-bold mb-4">Ex6 - Layout com Grid</h1>
-      {/* Resolva aqui */}
+      {<div className="grid grid-cols-1 max-h-[20rem]">
+        <div className="grid bg-amber-400 max-w-[full] max-h-[80px] justify-center">Cabeçalho</div>
+        <div className="grid bg-red-600 max-w-[full] max-h-[screen] justify-center">Conteúdo Central</div>
+        <div className="grid bg-green-600 max-w-[full] max-h-[80px] justify-center">Conteúdo Central</div>
+        </div>}
     </div>
   );
 }
