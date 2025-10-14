953c2aa4f5537d490c69d3201c3f6ac3c381b56e
Leonardo Lopes Pereira <leonardolopespereira7@gmail.com>
2025-10-14 20:00:39 -0300

    ex2 realizado

--- src/components/Ex2.jsx
+++ src/components/Ex2.jsx
@@ -6,7 +6,11 @@ export default function Ex2() {
   return (
     <div className="my-8">
         <h1 className="text-2xl font-bold mb-4">Ex2 - Flexbox Básico</h1>
-      {/* Resolva aqui */}
+      {<div className="flex justify-center align-middle gap-6">
+        <div className="w-12 h-12 bg-red-600"></div>
+        <div className="w-12 h-12 bg-blue-600"></div>
+        <div className="w-12 h-12 bg-green-600"></div>
+        </div>}
     </div>
   );
 }
