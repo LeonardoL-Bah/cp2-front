bf694fa7085d7c726782cf65b00769407509903f
Leonardo Lopes Pereira <leonardolopespereira7@gmail.com>
2025-10-14 19:50:12 -0300

    ex1 realizado

--- src/components/Ex1.jsx
+++ src/components/Ex1.jsx
@@ -6,7 +6,13 @@ export default function Ex1() {
   return (
     <div className="my-8">
         <h1 className="text-2xl font-bold mb-4">Ex1 - Botão Estilizado</h1>
-      {/* Resolva aqui */}
+      {
+        <div className="flex justify-center gap-6">
+          <button className="px-6 py-3 bg-blue-600 text-white rounded-lg animation:pulse hover:bg-blue-800 transition-colors">
+            Botão
+          </button>
+        </div>
+        }
     </div>
   );
 }
