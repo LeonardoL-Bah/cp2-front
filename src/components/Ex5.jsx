622b03dc61392446f0fd6a5548f73e9f1a28b492
Leonardo Lopes Pereira <leonardolopespereira7@gmail.com>
2025-10-14 20:49:08 -0300

    ex5 realizado

--- src/components/Ex5.jsx
+++ src/components/Ex5.jsx
@@ -9,7 +9,14 @@ export default function Ex5() {
   return (
     <div className="my-8">
         <h1 className="text-2xl font-bold mb-4">Ex5 - Navbar Responsiva</h1>
-      {/* Resolva aqui */}
+      {<aside className="w-56 bg-blue-800 text-white h-screen p-5 flex-col">
+        <nav className="flex flex-col gap-3">
+          <a>Home</a>
+          <a>Home</a>
+          <a>Home</a>
+          <a>Home</a>
+        </nav>
+        </aside>}
     </div>
   );
 }
