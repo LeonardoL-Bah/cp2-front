c9b1721e83e279ad2b1a30b1a76015cd0b25e91b
Leonardo Lopes Pereira <leonardolopespereira7@gmail.com>
2025-10-14 20:38:55 -0300

    ex4 realizado

--- src/components/Ex4.jsx
+++ src/components/Ex4.jsx
@@ -6,7 +6,13 @@ export default function Ex4() {
   return (
     <div className="my-8">
         <h1 className="text-2xl font-bold mb-4">Ex4 - Card Responsivo</h1>
-      {/* Resolva aqui */}
+      {<div className="flex-1  items-center w-90 flex flex-col bg-gray-400 width: 100%; @media (width >= 800px) {width: 100%}">
+        <h3>Título</h3>
+        <p className="">Matéria de front é massa</p>
+        <button className="px-6 py-3 bg-blue-600 text-white rounded-lg animation:pulse hover:bg-blue-800 transition-colors">
+            Botão
+          </button>
+        </div>}
     </div>
   );
 }
